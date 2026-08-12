6457de7cf1a686c57d01dcff2a77c703b2e21f40
Anil Kumar G R <anilkumargr27@gmail.com>
2026-08-12 18:49:47 +0530

    Time: 11 ms (36.82%) | Memory: 13.2 MB (49.34%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def jump(self, nums):
+        j=0
+        c=0
+        s=0
+        for i in range(0,len(nums)-1):
+            s=max(s,i+nums[i])
+            if i==c:
+                c=s
+                j+=1
+                if c==len(nums)-1:
+                    return j
+        return j
+
+        
+        
