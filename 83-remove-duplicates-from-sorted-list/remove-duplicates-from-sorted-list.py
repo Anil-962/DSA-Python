8298ac334a856ebe553189b426b13fbd5de98a92
Anil Kumar G R <anilkumargr27@gmail.com>
2026-08-20 10:00:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (30.26%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -5,10 +5,11 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        a = head
-        while a and a.next:
-            if a.val==a.next.val:
-                a.next = a.next.next
+        curr =head
+        while curr and curr.next:
+            if curr.val==curr.next.val:
+                curr.next=curr.next.next
             else:
-                a =a.next
+                curr = curr.next
         return head
+        
